e2ad79018b796d411db3b927962e0dc48b9b7e8b
Kishor Naik <kishor.naik011.net@gmail.com>
2019-07-31 15:43:25 +0530

    Code Commit

---
+++ Sol_SingleR_Chat/Sol_SingleR_Chat/Hubs/ChatHubs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Sol_SingleR_Chat.Hubs
+{
+    public class ChatHubs : Hub
+    {
+        public async Task SendMessageAsync(string userName, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessageMethod", userName, message);
+        }
+        
+    }
+}
